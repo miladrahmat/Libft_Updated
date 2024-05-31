c64a73a505eaddfedd93faf9d35da22a4440c9b4
Milad Rahmat Abadi <mrahmat-@student.hive.fi>
2024-05-31 11:15:57 +0300

    Libft + ft_printf

---
+++ ft_strchr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 12:28:58 by mrahmat-          #+#    #+#             */
+/*   Updated: 2024/05/08 10:33:00 by mrahmat-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int	i;
+	char			*ptr;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+		{
+			ptr = (char *)&s[i];
+			return (ptr);
+		}
+		else
+			i++;
+	}
+	if (s[i] == (char)c)
+	{
+		ptr = (char *)&s[i];
+		return (ptr);
+	}
+	else
+		return (0);
+}
